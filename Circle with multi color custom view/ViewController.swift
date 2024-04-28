2036dd9f2eff73b81b1f76ecbd97ef9d74073ba3
Pooyan J <pooyanjavidi@gmail.com>
2024-04-28 14:03:36 +0330

    Initial Commit

---
+++ Circle with multi color custom view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Circle with multi color custom view
+//
+//  Created by Pooyan J on 2/9/1403 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
